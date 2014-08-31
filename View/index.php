3ca8497fa9ab299bde4008e9b1aada3749cb623b
elitonperin <eliton_perin@ubuntu.ubuntu-domain>
2014-08-31 03:50:07 -0400

    Incluindo informações no página inicial.

--- View/index.php
+++ View/index.php
@@ -2,14 +2,34 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Armários da CACOM</title>
+        <title>CACOM</title>
     </head>
     <body>
-        <h1>Bem vindo ao cadastro de armários da CACOM</h1>
+        <h1>Bem vindo a CACOM</h1>
+        <h2>Quem somos</h2>
+        <p>Somos o centro acadêmico de Ciência da Computação da Faculdade de Computação da UFMS.</p>
+        <h2>Localização</h2>
+        <p>Nos localizamos próximo a sala de mestrado de Ciência da Computação. Você pode conferir melhor na imagem:</p>
+        <h2>História</h2>
+        <p>
+            A CACOM foi fundada no dia XX de XX de XXXX pelos alunos BLABLA. Deste tempo para lá 
+        </p>
+        <h2>Armários</h2>
+        <p>Somos responsáveis pela manutenção e aluguel dos armários situados no prédio da FACOM. 
+            São 152 armários disponíveis para qualquer pessoa que frequênte a faculdade. 
+            Para utilizar um armário por dois semestre, o usuário deverá pagar 10 reais e para utilizar por um semestre 5 reais.
+        </p>
         <p>Primeiro passo para alugar um armário da CACOM é se cadastrar no link abaixo:</p>
         <p>
             <a id="Novo" href="../Controller/controle.php?action=novo">Cadastrar-se</a>
         </p>
+        <h2>Por que cobramos?</h2>
+        <p>Cobramos para a manutenção tanto do próprios armários 
+            quanto para manter a CACOM limpa e com recursos para interesses dos alunos da turma de Ciência da Computalção, 
+            como aluguel de ônibus para viagens, materiais para o espaço físico da CACOM.</p>
+        <h2>Impressora</h2>
+        <p>Possuímos uma impressora no espaço físico da CACOM, para ser utilizado pelos alunos da FACOM e demais cursos. 
+            Cobramos um valor simbólico de 0,05 reais por folha impressa, apenas para comprar folhas e recarga do tuner.</p>
         <p>
             <a id="Lista" href="../Controller/controle.php?action=login">Área do adiministrador</a>
         </p>        
